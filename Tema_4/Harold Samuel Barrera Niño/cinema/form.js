66900c584176786d0a4d5fb6df2eedac8ed13189
HaroldBarreraNino <hsbarreran@outlook.com>
2025-04-10 16:52:57 -0500

    Init Actividad 4

---
+++ Tema_4/Harold Samuel Barrera Niño/cinema/form.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("form-login");
+const userNombreApellido = document.getElementsByName("user_nombre_apellido");
+const btnSubmit = document.getElementById("form_submit_btn");
+
+function loginAndRedirect(){
+    console.log(form);
+}
