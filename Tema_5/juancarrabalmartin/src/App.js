5d2222996c472ab81312123a3a03cf0172cfa534
jcam <jcam@jcamweb.com>
2025-04-25 09:47:53 +0200

    juancarrabal movielist v1.

---
+++ Tema_5/juancarrabalmartin/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <p>Lista de pel&#205;culas</p>
+            <Footer />
+            {/* otros componentes */}
+        </div>
+    );
+}
+
+export default App;
+
